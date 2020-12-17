8fefbae872a7e7bd7305354cfee876949ab6a1c4
Robert Baumgartner <R.Baumgartner-1@tudelft.nl>
2020-12-17 17:23:00 +0100

    Fix

--- docs/conf.py
+++ docs/conf.py
@@ -24,31 +24,6 @@ author = 'Sicco Verwer, Chris Hammerschmidt'
 # The full version, including alpha/beta/rc tags
 release = '0.0.3'
 
-# -- ReadTheDocs configs ------------------------------------------------------
-import subprocess, os
-
-def configureDoxyfile(input_dir, output_dir):
-    with open('Doxyfile.in', 'r') as file :
-        filedata = file.read()
-
-    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
-    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
-
-    with open('Doxyfile', 'w') as file:
-        file.write(filedata)
-
-# Check if we're running on Read the Docs' servers
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-breathe_projects = {}
-
-if read_the_docs_build:
-    input_dir = '../source'
-    output_dir = 'build'
-    configureDoxyfile(input_dir, output_dir)
-    subprocess.call('doxygen', shell=True)
-    breathe_projects['flexfringe'] = output_dir + '/xml'
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -87,3 +62,28 @@ html_static_path = ['_static']
 # -- Breathe configs ----------------------------------------------------------
 breathe_projects = {'flexfringe': 'doxygen/xml/'}
 breathe_default_project = 'flexfringe'
+
+# -- ReadTheDocs configs ------------------------------------------------------
+import subprocess, os
+
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+breathe_projects = {}
+
+if read_the_docs_build:
+    input_dir = '../source'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['flexfringe'] = output_dir + '/xml'
